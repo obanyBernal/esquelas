0828bd2906b230f75d297319ec8004f7104c0fcf
obany Bernal <obanybernal@iMac-de-obany.local>
2025-05-15 10:13:34 -0600

    Versión inicial para despliegue en Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/system/octo/esquelas/app/',
+  base: '/', // ✅ base vacía para que funcione bien en Vercel
   plugins: [react()],
-  build: {
-    outDir: "dist/system/octo/esquelas/app",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        vista: "vista.html", 
-      },
-    },
-  },
 })
